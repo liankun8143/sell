09d3f4506279071979869c6ea8c6f44c53efb883
liankun <250275747@qq.com>
2018-06-05 23:45:59 +0800

    selltest

---
+++ src/test/java/com/liankun/sell/logTest.java
@@ -0,0 +1,19 @@
+package com.liankun.sell;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@Slf4j
+@SpringBootTest
+public class logTest {
+
+    @Test
+    public void test2(){
+        log.info("123123123123");
+
+    }
+
+}
